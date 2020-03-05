21f21f1a60b6a8bef3ea77d65c9ae5e3fbee21e2
Lucas <lucas.masina1@gmail.com>
2020-03-05 23:34:50 +1100

    updated json handle

--- PHASE_1/API_SourceCode/API/disease_reports.py
+++ PHASE_1/API_SourceCode/API/disease_reports.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 from http import HTTPStatus
 
 DISEASE_REPORTS_BLUEPRINT = Blueprint("disease_reports", __name__)
 
 
-@DISEASE_REPORTS_BLUEPRINT.route('/disease_reports/<parameter>', methods=["GET"])
-def disease_reports(parameter):
+@DISEASE_REPORTS_BLUEPRINT.route('/disease_reports/<test_input>', methods=["GET"])
+def disease_reports(test_input):
+    parameter = request.json
+
     if parameter == {}:
         return jsonify({"invalid_request": "lol"}), HTTPStatus.BAD_REQUEST
 
-    return jsonify(parameter)
+    return jsonify(parameter, test_input)
