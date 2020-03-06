d839388229f2b9818e98a4080e31d46a92684dc2
Lucas <lucas.masina1@gmail.com>
2020-03-06 23:22:09 +1100

    added initial logging functionality

--- PHASE_1/API_SourceCode/API/disease_reports.py
+++ PHASE_1/API_SourceCode/API/disease_reports.py
@@ -1,14 +1,36 @@
 from flask import Blueprint, request, jsonify
 from http import HTTPStatus
+from PHASE_1.API_SourceCode.API.logger import log_api_request, user_log
+from datetime import datetime
+import time
 
 DISEASE_REPORTS_BLUEPRINT = Blueprint("disease_reports", __name__)
 
 
 @DISEASE_REPORTS_BLUEPRINT.route('/disease_reports/<test_input>', methods=["GET"])
 def disease_reports(test_input):
+    accessed_time = datetime.now()
+    request_start_time = time.perf_counter_ns()
     parameter = request.json
 
+    # testing purposes
+    if parameter is None:
+        parameter = {
+            "start_date": "test",
+            "end_date": "test",
+            "key_terms": "test",
+            "location": "location"
+        }
+
+    start_date = parameter.get("start_date")
+    end_date = parameter.get("end_date")
+    key_terms = parameter.get("key_terms")
+    location = parameter.get("location")
+
     if parameter == {}:
+        # TODO add error log
         return jsonify({"invalid_request": "lol"}), HTTPStatus.BAD_REQUEST
 
-    return jsonify(parameter, test_input)
+    request_end_time = time.perf_counter_ns()
+    log_api_request(request_end_time - request_start_time)
+    return jsonify("PARAMETER", parameter, "USER LOG", user_log(accessed_time), "ARGUMENT", test_input)
---
+++ PHASE_1/API_SourceCode/API/logger.py
@@ -0,0 +1,19 @@
+import logging
+from flask import request
+
+
+def user_log(accessed_time):
+    log = {
+            "team_name": "CalmClams",
+            "accessed_time": str(accessed_time),
+            "data_source": "outbreaks.globalincidentmap.com"
+    }
+    return log
+
+
+# end_point, process_time_taken, resource_utilization
+# write any monitoring data to a log file
+def log_api_request(process_time):
+    logging.basicConfig(filename='requests.log', level=logging.DEBUG)
+    logging.info("Endpoint accessed: " + str(request.endpoint))
+    logging.info("Process time: " + str(process_time) + "ns")
