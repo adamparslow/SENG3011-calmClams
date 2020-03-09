fc846d8874cdda8fc5f26d7c7d3f30c12343dea0
Marcus-Majchrzak <marcusmajchrzak@gmail.com>
2020-03-09 16:01:35 +1100

    Added React Framework + Typescript

---
+++ PHASE_2/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>Chuck our service in here!</h2>
+    </div>
+  );
+}
