2df50cdffebfbbf0536e06afbe897db729ab9fc8
carlinw <carlin.williamson1@gmail.com>
2020-03-13 12:53:20 +1100

    json formatting and better string cleaning

--- PHASE_1/API_SourceCode/Scraper/scrape_events.py
+++ PHASE_1/API_SourceCode/Scraper/scrape_events.py
@@ -3,35 +3,70 @@ import re
 from bs4 import BeautifulSoup
 from word2number import w2n
 import time
+import json
 
 BASE_URL = 'http://outbreaks.globalincidentmap.com/eventdetail.php?ID='
-starting_index = 30000
-ending_index = starting_index + 500
+starting_index = 101
+ending_index = starting_index + 36000
 
+short_description_header = re.compile(r'\[.*?\][A-Z\s]* [:-]+ ')
+leading_trailing_quotes = re.compile(r'"(.*)"')
 
-for i in range(starting_index, ending_index):
+# removes white space and unicode characters
+def clean(string):
+    #string = re.sub(r'\n', '. ', string)
+    string = re.sub(r'\r', '', string)
+    string = short_description_header.sub(r' ', string)
+    string = leading_trailing_quotes.sub(r'\1', string)
+    string = re.sub(r'^\.', r'', string)
+    """string.replace(' .','.')
+    string.replace(' .','.')
+    string.replace('..','.')
+    string.replace('..','.')
+    string.replace('..','.')"""
+    string.replace('  ', ' ')
+    return string.strip().encode('ascii', 'ignore').decode('ascii')
+
+for i in range(starting_index, ending_index, 99):
+    time.sleep(5)
+    print("")    
     print(BASE_URL + str(i))
     event_page = requests.get(BASE_URL + str(i))
     event_soup = BeautifulSoup(event_page.text, 'html.parser')
 
     details = event_soup.find_all('td', {'class': 'tdline'})
 
-    event_type = details[1].text.strip()
-    date = details[3].text.strip()
-    country = details[5].text.strip()
-    city = details[7].text.strip()
-    latitude = details[9].text.strip()
-    longitude = details[11].text.strip()
-    url = details[13].text.strip()
+    event_type = clean(details[1].text)
+    date = clean(details[3].text)
+    country = clean(details[5].text)
+    city = clean(details[7].text)
+    latitude = clean(details[9].text)
+    longitude = clean(details[11].text)
+    url = clean(details[13].text)
 
+    # if there is no data then just continue
+    if (url == ""):
+        continue
 
-    descriptions = event_soup.find_all('strong')[7:9]
-    short_description = descriptions[0].text.strip()
-    long_description = descriptions[1].text.strip()
+    description = clean(event_soup.find_all('tr', {'class': 'tdtext'})[0].text)
     
-    print("Disease: {}\nDate: {}\nLocation: {}, {}\nLat/Long: {},{}\nLink: {}\nShort: {}\n Long: {}\n\n".format(event_type, date, country, city, latitude, longitude, url, short_description, long_description))
+    #print("Disease: {}\nDate: {}\nLocation: {}, {}\nLat/Long: {},{}\nLink: {}\nShort: {}\n Long: {}\n\n".format(event_type, date, country, city, latitude, longitude, url, short_description, long_description))
 
-    time.sleep(3)    
+    ret = {"date_of_publication": date,
+           "url": url,
+           "main_text": description,
+           "reports": [{
+                "event_date": date,
+                "locations":[{
+                    "country": country,
+                    "location": city,
+                    "coords": "{}, {}".format(latitude, longitude)
+                }],
+                "diseases": [event_type],
+                "syndromes": [event_type]
+           }]
+           }
+    print(json.dumps(ret, indent=4, sort_keys=True))
 
 
 """
