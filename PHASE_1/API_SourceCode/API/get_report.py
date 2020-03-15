93b90554c1aa5ef162924504caf704e064072cc2
Lucas <lucas.masina1@gmail.com>
2020-03-15 17:13:39 +1100

    added hard coded test example

--- PHASE_1/API_SourceCode/API/get_report.py
+++ PHASE_1/API_SourceCode/API/get_report.py
@@ -7,6 +7,10 @@ def get_report(parameter, database):
     key_terms = parameter.get("key_terms")
     location = parameter.get("location")
 
+    # hard coded response
+    if key_terms == "test":
+        return dumps(test_result)
+
     # probably more processing required
 
     query = {
@@ -23,4 +27,30 @@ def get_report(parameter, database):
     return dumps(results)
 
 
-
+test_result = {
+    "url": "https://www.who.int/csr/don/17-january-2020-novel-coronavirus-japan-exchina/en/",
+    "date_of_publication": "2020-01-17 xx:xx:xx",
+    "headline": "Novel Coronavirus – Japan (ex-China)",
+    "main_text": "On 15 January 2020, the Ministry of Health, Labour and Welfare, Japan\n(MHLW) reported an imported case of laboratory-confirmed 2019-novel coronavirus (2019-\nnCoV) from Wuhan, Hubei Province, China. The case-patient is male, between the age of 30-\n39 years, living in Japan. The case-patient travelled to Wuhan, China in late December and\ndeveloped fever on 3 January 2020 while staying in Wuhan. He did not visit the Huanan\nSeafood Wholesale Market or any other live animal markets in Wuhan. He has indicated that\nhe was in close contact with a person with pneumonia. On 6 January, he traveled back to\nJapan and tested negative for influenza when he visited a local clinic on the same day.",
+    "reports": [
+        {
+            "event_date": "2020-01-03 xx:xx:xx to 2020-01-15",
+            "locations": [
+                {
+                    "country": "China",
+                    "location": "Wuhan, Hubei Province"
+                },
+                {
+                    "country": "Japan",
+                    "location": ""
+                }
+            ],
+            "diseases": [
+                "2019-nCoV"
+            ],
+            "syndromes": [
+                "Fever of unknown Origin"
+            ]
+        }
+    ]
+}
