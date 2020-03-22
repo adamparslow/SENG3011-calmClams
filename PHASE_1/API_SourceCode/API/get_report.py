98bf52049df93390d0bee0548203b4275ee8db94
Lucas <lucas.masina1@gmail.com>
2020-03-22 15:35:20 +1100

    minor refactor

--- PHASE_1/API_SourceCode/API/get_report.py
+++ PHASE_1/API_SourceCode/API/get_report.py
@@ -4,23 +4,20 @@ from datetime import datetime
 
 def get_report(parameter, database):
     start_date = parameter.get("start_date")
-    start_date = datetime.strptime(unquote(start_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if start_date else None
-
     end_date = parameter.get("end_date")
-    end_date = datetime.strptime(unquote(end_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if end_date else None
-
     key_terms = parameter.get("key_terms")
-    key_terms = unquote(key_terms).replace(" ", "").split(",") if key_terms else None
-    print(key_terms)
 
     location = parameter.get("location")            # process location
 
     query_list = []
 
     if start_date and end_date:
+        start_date = datetime.strptime(unquote(start_date).strip("\\\""), "%Y-%m-%d %H:%M:%S")  # TODO catch format errors
+        end_date = datetime.strptime(unquote(end_date).strip("\\\""), "%Y-%m-%d %H:%M:%S")      # TODO catch format errors
         query_list.append({"date_of_publication": {"$gte": start_date, "$lte": end_date}})
 
     if key_terms:
+        key_terms = unquote(key_terms).replace(" ", "").split(",")                              # TODO catch errors?
         for term in key_terms:
             term_query = {
                 "$or": [
@@ -33,7 +30,6 @@ def get_report(parameter, database):
     query = {"$and": query_list}
 
     results = list(database.find(query))
-    print("Number of results: ", len(results))
 
     if len(results) == 0:
         return None
