17e98fd29e8dcaf5151dd03412d17cc7197d1bc9
Lucas <lucas.masina1@gmail.com>
2020-03-22 18:01:14 +1100

    added kickass resource monitoring

--- PHASE_1/API_SourceCode/API/main.py
+++ PHASE_1/API_SourceCode/API/main.py
@@ -3,15 +3,17 @@ from pymongo import MongoClient
 from disease_reports import DISEASE_REPORTS_BLUEPRINT
 import dns
 from gql_endpoint import GRAPHQL_BLUEPRINT
+import flask_monitoringdashboard as dashboard
 
-APP = Flask(__name__)
+app = Flask(__name__)
 client = MongoClient('mongodb+srv://admin:admin@cluster0-zhnwq.gcp.mongodb.net/test?retryWrites=true&w=majority')
 db = client["disease_reports"]
 collection = db["beta"]
-APP.collection = collection
+app.collection = collection
+dashboard.bind(app)
 
-APP.register_blueprint(DISEASE_REPORTS_BLUEPRINT)
-APP.register_blueprint(GRAPHQL_BLUEPRINT)
+app.register_blueprint(DISEASE_REPORTS_BLUEPRINT)
+app.register_blueprint(GRAPHQL_BLUEPRINT)
 
 if __name__ == "__main__":
-    APP.run(host="127.0.0.1", port="5001")
+    app.run(host="127.0.0.1", port="5001")
