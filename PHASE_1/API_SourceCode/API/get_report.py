b4d3492e76a0cc36797240947ae9c68e521697c6
Lucas <lucas.masina1@gmail.com>
2020-03-22 15:27:40 +1100

    refactored get_report and added key_terms functionality

--- PHASE_1/API_SourceCode/API/get_report.py
+++ PHASE_1/API_SourceCode/API/get_report.py
@@ -4,24 +4,34 @@ from datetime import datetime
 
 def get_report(parameter, database):
     start_date = parameter.get("start_date")
-    start_date = datetime.strptime(unquote(start_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if start_date else None   # check format is same as in db
+    start_date = datetime.strptime(unquote(start_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if start_date else None
 
     end_date = parameter.get("end_date")
-    end_date = datetime.strptime(unquote(end_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if end_date else None         # check format is same as in db
+    end_date = datetime.strptime(unquote(end_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if end_date else None
 
-    key_terms = parameter.get("key_terms", "")      # process list of strings, check default
+    key_terms = parameter.get("key_terms")
+    key_terms = unquote(key_terms).replace(" ", "").split(",") if key_terms else None
+    print(key_terms)
 
     location = parameter.get("location")            # process location
 
-    print(start_date)
-    print(end_date)
-    query = {
-        "$and": [
-            {"date_of_publication": {"$gte": start_date, "$lte": end_date}},
-            #{"key_terms": key_terms},
-            #{"location": location}
-        ]
-    }
+    query_list = []
+
+    if start_date and end_date:
+        query_list.append({"date_of_publication": {"$gte": start_date, "$lte": end_date}})
+
+    if key_terms:
+        for term in key_terms:
+            term_query = {
+                "$or": [
+                    {"headline": {"$regex": term}},
+                    {"main_text": {"$regex": term, "$options": "i"}}
+                ]
+            }
+            query_list.append(term_query)
+
+    query = {"$and": query_list}
+
     results = list(database.find(query))
     print("Number of results: ", len(results))
 
