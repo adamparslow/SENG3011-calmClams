7a215ea62a8fcc43c3177700b5401fad92dad299
Lucas <lucas.masina1@gmail.com>
2020-03-22 14:43:55 +1100

    database query modifications

--- PHASE_1/API_SourceCode/API/get_report.py
+++ PHASE_1/API_SourceCode/API/get_report.py
@@ -7,18 +7,21 @@ def get_report(parameter, database):
     start_date = datetime.strptime(unquote(start_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if start_date else None   # check format is same as in db
 
     end_date = parameter.get("end_date")
-    end_date = datetime.strptime(unquote(end_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if end_date else None  # check format is same as in db
+    end_date = datetime.strptime(unquote(end_date).strip("\\\""), "%Y-%m-%d %H:%M:%S") if end_date else None         # check format is same as in db
 
     key_terms = parameter.get("key_terms", "")      # process list of strings, check default
 
-    location = parameter.get("location")    # process location
+    location = parameter.get("location")            # process location
+
     print(start_date)
     print(end_date)
-    query = {"$and": [
-        {"articles.date_of_publication": {"$gte": start_date, "$lt": end_date}},
-        #{"key_terms": key_terms},
-        #{"location": location}
-    ]}
+    query = {
+        "$and": [
+            {"articles.date_of_publication": {"$gte": start_date, "$lte": end_date}},
+            #{"key_terms": key_terms},
+            #{"location": location}
+        ]
+    }
     results = list(database.find(query))
     print("Number of results: ", len(results))
 
