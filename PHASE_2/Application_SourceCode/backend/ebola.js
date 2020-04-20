bac7ec57297c3e347b2d69b59becc268f4d1af8e
Lucas <lucas.masina1@gmail.com>
2020-04-20 18:06:10 +1000

    initial ebola data processing

--- PHASE_2/Application_SourceCode/backend/ebola.js
+++ PHASE_2/Application_SourceCode/backend/ebola.js
@@ -1,12 +1,17 @@
-import readCSV from 'zebras'
+// import readCSV, {head} from 'zebras'
+const z = require('zebras');
 
 const handleEbola = () => {
     console.error("Not implemented");
+    getEbolaData();
 };
 
 const getEbolaData = () => {
-    const data = readCSV('Ebola Data.csv')
+    const data = z.readCSV('Ebola Data.csv')
+    console.log(z.head(3, data))
     return data;
 };
+
+handleEbola();
 
 module.exports = handleEbola;
