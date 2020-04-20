f9175ef389baac9e178379392bf0db8c6b512d21
Lucas <lucas.masina1@gmail.com>
2020-04-20 17:46:43 +1000

    start  of ebola csv processing

--- PHASE_2/Application_SourceCode/backend/ebola.js
+++ PHASE_2/Application_SourceCode/backend/ebola.js
@@ -1,5 +1,12 @@
+import readCSV from 'zebras'
+
 const handleEbola = () => {
     console.error("Not implemented");
+};
+
+const getEbolaData = () => {
+    const data = readCSV('Ebola Data.csv')
+    return data;
 };
 
 module.exports = handleEbola;
