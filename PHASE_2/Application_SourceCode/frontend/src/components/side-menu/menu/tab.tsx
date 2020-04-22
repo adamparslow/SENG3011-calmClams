85faf9b45770609e44df533e96d95c0a47c1e94f
Marcus-Majchrzak <marcusmajchrzak@gmail.com>
2020-04-22 20:38:53 +1000

    Added navigation

--- PHASE_2/Application_SourceCode/frontend/src/components/side-menu/menu/tab.tsx
+++ PHASE_2/Application_SourceCode/frontend/src/components/side-menu/menu/tab.tsx
@@ -1,24 +1,25 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
-import config from '../../../config'
+import config from '../../../config';
 
 const StyledTab = styled.div`
-
+  color: ${config.theme.primaryLight};
+  background-color: ${({ active }) =>
+    active ? config.theme.mediumColor : config.theme.darkColor};
+  padding: 20px;
+  font-size: 20px;
+  text-align: center;
 `;
 
 interface tabType {
-    tab: number, 
-    setTab: Dispatch<SetStateAction<number>>
-  }
+  tab: number;
+  setTab: Dispatch<SetStateAction<number>>;
+}
 
-
-const Tab = ({tab, setTab}) => {
+const Tab = ({ tab, setTab, tabName, active, open }) => {
   return (
-    <StyledTab
-      onClick={setTab(tab)}
-    >
-        THIS IS A TAB
-      {/* <span>{props.children}</span> */}
+    <StyledTab active={active} onClick={() => open && setTab(tab)}>
+      {tabName}
     </StyledTab>
   );
 };
