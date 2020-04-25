0d511e5ee81d6cd36d15e822e0b82a536668448a
Lucas <lucas.masina1@gmail.com>
2020-04-25 20:42:40 +1000

    moved to autocomplete component

--- PHASE_2/Application_SourceCode/frontend/src/pages/search-page/searchPanel.tsx
+++ PHASE_2/Application_SourceCode/frontend/src/pages/search-page/searchPanel.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../components/button';
-import Input from '../../components/input';
+import Autocomplete from '../../components/autocomplete';
 import Modal from '../../components/modal';
-import TextField from '../../components/autocomplete';
 import config from '../../config';
 import DateInput from '../../components/date-picker';
-//import Autocomplete from '@material-ui/lab/Autocomplete';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-//import TextField from '@material-ui/core/TextField';
+
 
 const FlexContainer = styled.div`
   display: flex;
@@ -25,17 +22,6 @@ const GridContainer = styled.div`
   text-align: center;
 `;
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: 500,
-      '& > * + *': {
-        marginTop: theme.spacing(3),
-      },
-    },
-  }),
-);
-
 interface SearchPanelProps {
   fetchData: (
     searchquery: string,
@@ -44,7 +30,6 @@ interface SearchPanelProps {
 }
 
 export const SearchPanel = (props: SearchPanelProps) => {
-  const classes = useStyles();
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [keyTerms, setKeyTerms] = useState('');
@@ -115,43 +100,10 @@ export const SearchPanel = (props: SearchPanelProps) => {
         <DateInput onChange={handleEndDateChange} width={200} />
       </GridContainer>
       <GridContainer>
-      <Autocomplete
-        multiple
-        id="tags-standard"
-        options={key_terms}
-        getOptionLabel={(option) => option}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="standard"
-            label="KEY TERMS"
-            placeholder={'Key Terms'}
-            onChange={handleKeyTerms}
-            //toolTipTitle={'Help'}
-            //toolTipMessage={'Type out your key terms separated by commas.\nAll reports relating to those countries will be displayed on the map and below in the reports section.\nIf left blank, it will search for all reports.'}
-          />
-        )}
-      />
+        <Autocomplete onChange={handleKeyTerms} options={'key_terms'} label={'KEY TERMS'} placeholder={'Key Terms'} />
       </GridContainer>
       <GridContainer>
-        <Autocomplete
-        multiple
-        id="tags-standard"
-        options={countries}
-        getOptionLabel={(option) => option}
-        defaultValue={[countries[0]]}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="standard"
-            label="LOCATIONS"
-            placeholder={'Locations'}
-            onChange={handleLocation}
-            //toolTipTitle={'Help'}
-            //toolTipMessage={'Type out your countries separated by commas.\nAll reports relating to those countries will be displayed on the map and below in the reports section.\nIf left blank, it will search for all reports.'}
-          />
-        )}
-      />
+        <Autocomplete onChange={handleKeyTerms} options={'countries'} label={'COUNTRIES'} placeholder={'Countries'} />
       </GridContainer>
       <GridContainer>
         <Button hover={true} onClick={santitisedDataFetch}>
@@ -161,8 +113,5 @@ export const SearchPanel = (props: SearchPanelProps) => {
     </FlexContainer>
   );
 };
-
-const countries = ["Global","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-const key_terms = ["Rift Valley Fever","Crimean Congo Hemorrhagic Fever","Dengue","Ebola","Marburg","Zika","MERS","Salmonella","Legionnaire","Measles","Anthrax","Botulism","Plague","Smallpox","Tularemia","Junin Fever","Machupo Fever","Guanarito Fever","Chapare Fever","Lassa Fever","Lujo Fever","Hantavirus"];
 
 export default SearchPanel;
