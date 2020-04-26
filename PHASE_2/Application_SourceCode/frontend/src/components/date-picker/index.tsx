3de45517864cdf9bf366c0a471cfa7f53fcb752e
Lucas <lucas.masina1@gmail.com>
2020-04-26 21:25:11 +1000

    Modified date css

--- PHASE_2/Application_SourceCode/frontend/src/components/date-picker/index.tsx
+++ PHASE_2/Application_SourceCode/frontend/src/components/date-picker/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 //TODO: Add typescript typing
 const Input = styled.input`
   width: ${({ width }) => (width ? `${width}px` : `100%`)};
-  height: 25px;
+  height: 30px;
   border-radius: ${({ enableButton }) =>
     enableButton ? '5px 0px 0px 5px' : '5px 5px 5px 5px'};
   border: none;
